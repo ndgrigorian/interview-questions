bcaa67e7c93483f76f0539bb521a56eb3531a597
ndgrigorian <46709016+ndgrigorian@users.noreply.github.com>
2022-02-02 15:31:37 -0800

    Update solution.py
    Object-oriented: suffix_tree object

--- fuzzy_phrases/solution.py
+++ fuzzy_phrases/solution.py
@@ -2,42 +2,48 @@ import json
 from collections import deque
 
 
-def phrasel_search(P, Queries):
-    # Write your solution here
-    ans = []
-    suffix_tree = {}
-    extra_token_limit = 1
-    for phrase in P:
-        tree = suffix_tree
-        #replacing all '$' in our strings handles possibility of interfering with
-        #use of '$' as phrase-ending character
-        phrase.replace('$', '')
-        words = phrase.split(' ')
-        words.append('$')
-        for word in words:
-            tree = tree.setdefault(word, {})
-    for query in Queries:
-        query_tokens = query.split(' ')
+class SuffixTree:
+
+    class Node:
+        def __init__(self, lab):
+            self.label = lab
+            self.next_tokens = {}
+
+    def __init__(self, phrases):
+        self.root = self.Node(None)
+        for phrase in phrases:
+            # replacing all '$' in our strings handles possibility of interfering with
+            # use of '$' as phrase-ending character
+            phrase.replace('$', '')
+            tokens = phrase.split(' ')
+            tokens.append('$')
+            current_node = self.root
+            for token in tokens:
+                if not current_node.next_tokens.get(token):
+                    current_node.next_tokens[token] = self.Node(token)
+                    current_node = current_node.next_tokens[token]
+                else:
+                    current_node = current_node.next_tokens[token]
+
+    def tree_query(self, query, extra_token_limit = 0):
         query_phrases = []
-        for start_index, query_token in enumerate(query_tokens):
-            tree = suffix_tree
+        for start_index, token in enumerate(query):
             current_phrase = deque()
             current_index = start_index
+            current_phrase.append(token)
+            current_node = self.root
             extra_tokens = 0
-            current_phrase.append(query_token)
-            
-            #loop breaks if the current phrase queue is empty for safety
             while current_phrase:
                 current_word = current_phrase.pop()
-                if tree.get(current_word):
-                    tree = tree.get(current_word)
+                if current_node.next_tokens.get(current_word):
+                    current_node = current_node.next_tokens.get(current_word)
                     current_phrase.append(current_word)
-                    if tree.get("$") == {}:
+                    if current_node.next_tokens.get("$"):
                         # the following conditional is necessary -- it handles cases of degenerate phrases
                         # i.e., a phrase 'field example' and 'field example pop' should return
                         # field example and field example pop. In degenerate cases,
-                        #more than 1 key will be present.
-                        if len(tree.keys()) == 1:
+                        # more than 1 key will be present.
+                        if len(current_node.next_tokens.keys()) == 1:
                             query_phrases.append(" ".join(list(current_phrase)))
                             break
                         else:
@@ -49,12 +55,24 @@ def phrasel_search(P, Queries):
                     current_phrase.append(current_word)
 
                 current_index += 1
-                if current_index < len(query_tokens):
-                    current_phrase.append(query_tokens[current_index])
+                if current_index < len(query):
+                    current_phrase.append(query[current_index])
+        return query_phrases
+
+
+def phrasel_search(P, Queries):
+    # Write your solution here
+    ans = []
+    extra_token_limit = 1
+    suffix_tree = SuffixTree(P)
+
+    for query in Queries:
+        query_tokens = query.split(' ')
+        query_phrases = suffix_tree.tree_query(query_tokens, extra_token_limit)
         if query_phrases:
             ans.append(query_phrases)
-
     return ans
+
 
 if __name__ == "__main__":
     with open('sample.json', 'r') as f:
