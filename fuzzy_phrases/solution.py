3a9277ff25143758fd1ba8609ddbc0b98b8ebd24
ndgrigorian <46709016+ndgrigorian@users.noreply.github.com>
2022-02-02 12:04:23 -0800

    Update solution.py
    Changed several variable names to use token rather than word
    Added logic to handle case of degenerate phrases -- i.e., if a phrase is 'log error' and another is 'log error two,' it will correctly parse out both

--- fuzzy_phrases/solution.py
+++ fuzzy_phrases/solution.py
@@ -6,48 +6,54 @@ def phrasel_search(P, Queries):
     # Write your solution here
     ans = []
     suffix_tree = {}
-    
-    #build a suffix-tree-like structure from the given phrases
-    #the keys of the next dictionary are the possible next words
-    #a next key of "$" indicates the previous word was terminal for a phrase
+    extra_token_limit = 1
     for phrase in P:
         tree = suffix_tree
+        #replacing all '$' in our strings handles possibility of interfering with
+        #use of '$' as phrase-ending character
+        phrase.replace('$', '')
         words = phrase.split(' ')
         words.append('$')
         for word in words:
             tree = tree.setdefault(word, {})
-            
-    #now iterate over queries, splitting up 
     for query in Queries:
         query_tokens = query.split(' ')
         query_phrases = []
-        
         for start_index, query_token in enumerate(query_tokens):
             tree = suffix_tree
             current_phrase = deque()
             current_index = start_index
-            extra_words = 0
+            extra_tokens = 0
+            current_phrase.append(query_token)
             
-            while current_index < len(query_tokens):
-                current_phrase.append(query_tokens[current_index])
-                test_word = current_phrase.pop()
-                
-                if tree.get(test_word):
-                    tree = tree.get(test_word)
-                    
+            #loop breaks if the current phrase queue is empty for safety
+            while current_phrase:
+                current_word = current_phrase.pop()
+                if tree.get(current_word):
+                    tree = tree.get(current_word)
+                    current_phrase.append(current_word)
                     if tree.get("$") == {}:
-                        current_phrase.append(test_word)
-                        query_phrases.append(" ".join(list(current_phrase)))
-                        break
+                        # the following conditional is necessary -- it handles cases of degenerate phrases
+                        # i.e., a phrase 'field example' and 'field example pop' should return
+                        # field example and field example pop. In degenerate cases,
+                        #more than 1 key will be present.
+                        if len(tree.keys()) == 1:
+                            query_phrases.append(" ".join(list(current_phrase)))
+                            break
+                        else:
+                            query_phrases.append(" ".join(list(current_phrase)))
                 else:
-                    extra_words += 1
-                    if not current_phrase or extra_words > 1:
+                    extra_tokens += 1
+                    if not current_phrase or extra_tokens > extra_token_limit:
                         break
-                current_phrase.append(test_word)
+                    current_phrase.append(current_word)
+
                 current_index += 1
-                
+                if current_index < len(query_tokens):
+                    current_phrase.append(query_tokens[current_index])
         if query_phrases:
             ans.append(query_phrases)
+
     return ans
 
 if __name__ == "__main__":
